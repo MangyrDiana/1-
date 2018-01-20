fb3d0b63d2d41c1daf4036c0fd00a60d3922af18
Diana Mangyr <34777277+MangyrDiana@users.noreply.github.com>
2018-01-20 20:35:20 +0700

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,15 +1,16 @@
 # импорт модуля
-import requests
+from requests import get
 import re
 
-r = requests.get('http://www.tsu.ru/education/faculties/')
-code=r.text
+r = get('http://www.tsu.ru/education/faculties/')
+text_code=r.text
+
+emails = re.findall(r"[\w.]+@\w+\.\w+", text_code) # возвращает список найденных фрагментов
+emails = set(emails) # множество адресов без повторений
+print(len(emails))
+print(emails) # вывод
+
 
 '''\w - любая буква,цифра и _
   + - 1 или более вхождений символа в строку
   . - любой символ, кроме новой строки \n'''
-reg = "[\w.]+@\w+\.\w+" # регулярное выражение, которое находит в тексте адреса
-emails = re.findall(reg, code) # возвращает список найденных фрагментов
-emails = set(emails) # множество эл. адресов без повторений
-print(emails) # вывод
-print(len(emails))
